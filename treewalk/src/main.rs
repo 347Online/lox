3b67c26fb9b364f4b7d6d2c2c368dd1cda7c2321
347Online | Katie Janzen <katiejanzen@347online.me>
2026-01-06 22:28:09 -0600

    Better fix for args behavior (since we don't care about the process path)

--- treewalk/src/main.rs
+++ treewalk/src/main.rs
@@ -2,20 +2,20 @@ use treewalk::exit::TOO_MANY_ARGS;
 use treewalk::lox::Lox;
 
 fn main() -> std::io::Result<()> {
-    let mut args = std::env::args();
+    let mut args = std::env::args().skip(1);
     let len = args.len();
 
-    if len > 2 {
+    if len > 1 {
         eprintln!("Usage: treewalk [script]");
         std::process::exit(TOO_MANY_ARGS);
     }
 
     let mut lox = Lox::new();
 
-    if len == 2 {
+    if len == 1 {
         let path = args.next().unwrap();
         lox.run_file(&path)?;
-    } else if len == 1 {
+    } else {
         lox.run_prompt()?;
     }
 
