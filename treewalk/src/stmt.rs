d207545358f464cb1c3951650f73cba0158b68d1
347Online | Katie Janzen <katiejanzen@347online.me>
2026-01-08 20:40:06 -0600

    Add SubStmt type, mirroring SubExpr

--- treewalk/src/stmt.rs
+++ treewalk/src/stmt.rs
@@ -1,3 +1,6 @@
+use std::fmt::Debug;
+use std::ops::{Deref, DerefMut};
+
 use crate::expr::Expr;
 use crate::token::Token;
 
@@ -7,4 +10,45 @@ pub enum Stmt<'src> {
     Expr(Expr<'src>),
     Print(Expr<'src>),
     Var(Token<'src>, Option<Expr<'src>>),
+}
+
+#[derive(Clone)]
+pub struct SubStmt<'a>(Box<Stmt<'a>>);
+
+impl Debug for SubStmt<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl<'a> From<Stmt<'a>> for SubStmt<'a> {
+    fn from(value: Stmt<'a>) -> Self {
+        SubStmt(Box::new(value))
+    }
+}
+
+impl<'a> AsRef<Stmt<'a>> for SubStmt<'a> {
+    fn as_ref(&self) -> &Stmt<'a> {
+        self.0.as_ref()
+    }
+}
+
+impl<'a> AsMut<Stmt<'a>> for SubStmt<'a> {
+    fn as_mut(&mut self) -> &mut Stmt<'a> {
+        self.0.as_mut()
+    }
+}
+
+impl<'a> Deref for SubStmt<'a> {
+    type Target = Stmt<'a>;
+
+    fn deref(&self) -> &Self::Target {
+        self.0.deref()
+    }
+}
+
+impl<'a> DerefMut for SubStmt<'a> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.0.deref_mut()
+    }
 }
